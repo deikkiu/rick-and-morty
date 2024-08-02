50f24a56f968c2aacc706d2bc29ba87f4c790bd3
Timur Smagulov <deikku@mail.ru>
2024-08-02 06:32:17 +0600

    feat: api service

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,5 +1,32 @@
 <template>
 	<h1 className="text-3xl font-bold underline">Home Page</h1>
+
+	<div class="mt-10">
+		<ul id="array-rendering">
+			<li v-for="item in list" v-bind:key="item.id">
+				{{ item.name }}
+			</li>
+		</ul>
+	</div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue';
+import {
+	getCharactersByPage,
+	getCharactersById,
+} from '@/api/character/character';
+
+const list = ref([]);
+
+async function fetchData() {
+	try {
+		const data = await getCharactersByPage(1);
+		list.value = data.results;
+	} catch (e) {
+		throw new Error(e);
+	}
+}
+
+onMounted(fetchData);
+</script>
